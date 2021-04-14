20ee6727765b2a1dc0efc3092c4fcad93310b5ed
Pawan Singh Chauhan <pawansinghchauhan@Pawans-MacBook-Air.local>
2021-04-15 02:30:12 +0530

    added functionalities for non threaded io

--- ObjectTracker.py
+++ ObjectTracker.py
@@ -36,5 +36,57 @@ while True:
     
     # if no frame is detected
     if frame == None:
-        
+        OT_log.addLog('error','no frames detected')
         break
+
+    # resize the frame, blur it, and convert it to the HSV color space
+    frame = imutils.resize(frame,width=600)
+	blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+	hsv = cv2.cvtColor(blurred, cv2.COLOR_BGR2HSV)
+
+    # construct a mask for the color selected, then perform
+	# a series of dilations and erosions to remove any small blobs left in the mask
+	mask = cv2.inRange(hsv, LowerHSV, UpperHSV)
+	mask = cv2.erode(mask, None, iterations=2)
+	mask = cv2.dilate(mask, None, iterations=2)
+
+    # find contours in the mask and initialize the current
+	# (x, y) center of the ball
+	cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL,
+		cv2.CHAIN_APPROX_SIMPLE)
+	cnts = imutils.grab_contours(cnts)
+	center = None
+    
+	# only proceed if at least one contour was found
+	if len(cnts) > 0:
+		# find the largest contour in the mask, then use
+		# it to compute the minimum enclosing circle and
+		# centroid
+		c = max(cnts, key=cv2.contourArea)
+		((x, y), radius) = cv2.minEnclosingCircle(c)
+		M = cv2.moments(c)
+		center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+    	# update the points queue
+    
+	pts.appendleft(center)
+# pop the cordinates and stream them
+    for i in range(1,len(pts)):
+        print(pts[i], end=" ")
+    
+
+    
+#STOPING THE CAMERA FEED
+
+	key = cv2.waitKey(1) & 0xFF
+	# if the 'q' key is pressed, stop the loop
+	if key == ord("q"):
+        OT_log.addLog('warning','closing the camera feed')
+		break
+
+
+#stop the camera video stream
+vs.stop()
+
+# close all windows
+cv2.destroyAllWindows()
