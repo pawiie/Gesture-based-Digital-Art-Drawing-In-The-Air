b4ef48d17d4f8139a6d2c468ad2dcf3f4e08338b
Pawan Singh Chauhan <pawansinghchauhan@Pawans-MacBook-Air.local>
2021-04-15 16:28:32 +0530

    added fps class to calculate the fps utilised

---
+++ fps.py
@@ -0,0 +1,35 @@
+"""
+Created on Thu Apr 15 2021 16:09:08
+
+@author: PS Chauhan
+"""
+
+# this module is to find the fps of the VideoStream 
+
+import datetime
+
+class FPS:
+    def __init__(self):
+        self._start = None
+        self._end = None
+        self._numframes = 0
+    
+    def start(self):
+        self._start = datetime.datetime.now()
+        return self
+    
+    def end(self):
+        self._end = datetime.datetime.now()
+    
+    def update(self):
+        #update the total number of frames in the start and end interval
+
+        self._numframes += 1
+
+    def elapsed(self):
+        #returns the time interval
+        return(self._end - self._start).total_seconds()
+
+    def fps(self):
+        return self._numframes/self.elapsed()
+     
