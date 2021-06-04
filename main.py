083f2a8ab993b491549d23cb2e660b32c1a1a455
Pawan Singh Chauhan <pawansinghchauhan@Pawans-MacBook-Air.local>
2021-06-04 07:05:36 +0530

    added client socket

--- main.py
+++ main.py
@@ -6,9 +6,14 @@ Created on Thu Apr 20 2021 15:44:36
 
 
 from flask import Flask, render_template
+from coord import liveData as ld
+
 app = Flask(__name__)
 
 @app.route('/')
-
 def hello():
-    return render_template('index.html')
+    return render_template('index.html',data = (coordinates for coordinates in ld()))
+
+
+
+
